e47e9618d6670692ccab12171be7467a32276201
Anton Patrushev <apatrushev@gmail.com>
2024-03-20 03:07:07 +0500

    style: cargo fmt

--- crates/ruff_linter/src/rules/wemake_python_styleguide/many_conditions.rs
+++ crates/ruff_linter/src/rules/wemake_python_styleguide/many_conditions.rs
@@ -10,23 +10,31 @@ pub struct TooManyConditions(usize);
 impl Violation for TooManyConditions {
     #[derive_message_formats]
     fn message(&self) -> String {
-        format!("Found a condition with too much logic:: ({} > {})", self.0, MAX_CONDITIONS)
+        format!(
+            "Found a condition with too much logic:: ({} > {})",
+            self.0, MAX_CONDITIONS
+        )
     }
 }
 
 fn count_conditions(bool_op: &ExprBoolOp) -> usize {
-    bool_op.values.iter().map(|x| {
-        match x {
+    bool_op
+        .values
+        .iter()
+        .map(|x| match x {
             ruff_python_ast::Expr::BoolOp(nested) => count_conditions(nested),
-            _ => 1
-        }
-    }).sum()
+            _ => 1,
+        })
+        .sum()
 }
 
 pub(crate) fn too_many_conditions(bool_op: &ExprBoolOp) -> Option<Diagnostic> {
     let conditions = count_conditions(bool_op);
     if conditions > MAX_CONDITIONS {
-        Some(Diagnostic::new(TooManyConditions(conditions), bool_op.range))
+        Some(Diagnostic::new(
+            TooManyConditions(conditions),
+            bool_op.range,
+        ))
     } else {
         None
     }
